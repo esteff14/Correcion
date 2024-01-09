0546a6f58f8ce3b35a0fe651ddde8265948fb1bc
Estefanía Sánchez <133397610+esteff14@users.noreply.github.com>
2024-01-08 21:10:40 -0500

    Update2 Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,31 @@ public class Main {
         Deporte juegoDe1 = new Deporte("8 ball pool", "pool", "pool", 96000);
 
         try {
-
-            juegoAv[0] = "Juego 0";
-            juegoAv[1] = "Juego 1";
-            juegoAv[2] = "Juego 2";
-
             for (int i = 0; i < 3; i++) {
                 System.out.println(juegoAv[i]);
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("No se puede");
         } finally {
-            juegoAv[0] = juegoAv1.getNombre();
-            juegoAv[1] = juegoAv1.getTipo();
+            juegoAv[0] = juegoAv1.getAutor();
+            juegoAv[1] = juegoAv1.getNombre();
+            juegoAv[2] = juegoAv1.getTipo();
+
             for (int i = 0; i < juegoAv.length; i++) {
                 System.out.println(juegoAv[i]);
             }
+            juegoAv1.imprimirVideojuego();
+            juegoAv1.tipoAventura();
         }
+        juegoDe1.setTipoDeporte("Pool");
+        juegoDe[0] = juegoDe1.getNombre();
+        juegoDe[1] = juegoDe1.getTipo();
+        juegoDe[2] = String.valueOf(juegoDe1.getCosto());
+
+        for(int i =0; i< juegoDe.length;i++){
+            System.out.println(juegoDe[i]);
+        }
+        juegoDe1.imprimirVideojuego();
+        juegoDe1.imprimirDep();
     }
 }
